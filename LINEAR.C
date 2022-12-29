d5eb0d806f80642226664632e61753a35d2fe4da
Rudra Malvankar <93910093+RUDR027@users.noreply.github.com>
2022-12-29 20:51:07 +0530

    Update LINEAR.C

--- LINEAR.C
+++ LINEAR.C
@@ -23,3 +23,26 @@ void main()
         printf("Element not found");
      getch();
 }
+
+
+// ALGO 
+
+//Step 1: Start
+
+//Step 2: Accept a values from user i.e. array elements.
+
+
+//Step 3. Accept element to be searched from user i.e. turget.
+
+//Step 4: Set i-0, flag-0
+
+//Step 5: Compare A[i] with target If(A[i] is a target)
+
+//Step 6: Set flag-1 go to step 7 Else Move to next data element i=i+1.
+
+//Step 7: If (in) go to step 5
+
+//Step 8: If(flag-1) then Retum as position of target located Else Report as target no found.
+
+//Step 9. Stop
+
